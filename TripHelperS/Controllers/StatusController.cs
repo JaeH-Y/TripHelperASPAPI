5c2a8d1c397198761d370a8f779d82d3525c87c0
JaeH-Y <yjh21175753@gmail.com>
2025-11-11 22:28:37 +0900

    Trip Server First Upload

---
+++ TripHelperS/Controllers/StatusController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TripHelperS.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StatusController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetServerTime()
+        {
+            DateTime now = DateTime.Now;
+
+            var response = new
+            {
+                serverTime = now.ToString("yyyy-MM-dd HH:mm:ss"),
+                timeZone = TimeZoneInfo.Local.StandardName
+            };
+
+            return Ok(response);
+        }
+
+        [HttpGet("utc")]
+        public IActionResult GetUtcTime()
+        {
+            var utcNow = DateTime.UtcNow;
+
+            return Ok(new
+            {
+                serverTimeUtc = utcNow.ToString("yyyy-MM-dd HH:mm:ss"),
+                timeZone = "UTC"
+            });
+        }
+    }
+}
